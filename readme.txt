f9a89789a062fa8684cbd2435aeeabc072b853c9
hoafelizz <hoa.nguyen@felizz.com>
2017-04-11 10:52:33 +0700

    Add LICENSE.txt, readme.txt

---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Seanote ===
+
+Tags: Social polls
+Requires at least: 3.7
+Tested up to: 4.7.3
+Stable tag: 3.3
+License: GPLv2 or later
+
+== Description ==
+
+Seanote embed seanote.com polls on your Wordpress sites.
+
+
+== Installation ==
+
+Upload the Seanote plugin to your blog, Activate it, then enter your [seanote id='poll_id'].
+Your can find poll id on seanote.com by click 'embed' on poll
+
+== Changelog ==
+
+= 1.0.0 =
+*Release Date - April 11, 2017*
+
+* Add embed css.
+* Add embed js.
+* Add shortcode to post.
+
--- seanote.php
+++ seanote.php
@@ -2,32 +2,30 @@
 /**
  * Plugin Name: Social Polls by Seanote.com
  * Plugin URI:  http://seanote.com
- * Description: Seanote are polls you can embed on your WordPress page. Engage your audience by asking them a question via Seanote.
- * Version:     1.4.7
+ * Description: Seanote are polls you can embed on your WordPress page. 
+ * Version:     1.0.0
  * Author:      Seanote Team
- * Author URI:  https://www.Seanote.com
- * License:     GPL3
- *
- * Seanote (WordPress Plugin)
+ * Author URI:  https://www.seanote.com
+ * License: GPLv2 or later
  *
  * @package   Seanote_Shortcode
- * @version   1.0.0
  * @copyright Copyright (C) 2017 Seanote
- * @link      https://www.seanote.com
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+*/
  
 // Make sure we don't expose any info if called directly
 if ( !function_exists( 'add_action' ) ) {
@@ -36,11 +34,11 @@ if ( !function_exists( 'add_action' ) ) {
 }
 
 function seanote_scripts() {
-  wp_enqueue_style('seanote-css','http://localhost:3000/static/css/embed.css');
-  wp_enqueue_script( 'seanote-plugin','http://localhost:3000/static/js/plugin.js', array('jquery'),'',true);
+  wp_enqueue_style('seanote-css','https://dev.seanote.com/static/css/embed.css');
+  wp_enqueue_script( 'seanote-plugin','https://dev.seanote.com/static/js/plugin.js', array('jquery'),'',true);
 }
 
-add_action( 'wp_enqueue_scripts', 'seanote_scripts' );
+add_action('wp_enqueue_scripts', 'seanote_scripts' );
 
 function seanote_shortcode($atts) {
   $embed = sprintf(
